b388238a32214635e0452cb3a0b94dc5a1403656
bnieveld <bas.nieveld@topicus.nl>
2024-02-06 09:14:25 +0100

    Add CR Touch support

--- BLTouch_Tester_v1.0/BLTouch_Tester_v1.0.ino
+++ BLTouch_Tester_v1.0/BLTouch_Tester_v1.0.ino
@@ -5,12 +5,20 @@
   Connect Pin 9 to Orange / Yellow
   Connect Pin 2 to White.
 
+  CRTouch Test sensor program
+  Connect Gnd to White
+  Connect 5V to Black
+  Connect Pin 9 to Yellow
+  Connect Gnd to Red
+  Connect Pin 2 to Blue
+
   Serial port to 9600 and dont send CR or LF
 
   Send 1 to Pin Down
   Send 2 to Pin Up
   Send 3 to Test
-  Send 4 to Reset
+  Send 4 to SW Reset
+  Send 5 to Reset
 
 */
 
@@ -21,8 +29,8 @@ Servo myservo;
 int val;  
 int incomingByte = 0;
 
-const byte BLTouchPin = 2;  // Connect the white wire from the BLTouch to this pin
-const byte BLTouchControl = 9;   // Connect the orange wire from the BLTouch sensor to this pin
+const byte BLTouchPin = 2;        // Connect the white wire from the BLTouch to this pin / crtouch blue
+const byte BLTouchControl = 9;    // Connect the orange wire from the BLTouch sensor to this pin / crtouch yellow
 volatile byte state = LOW;
 
 void setup() {
@@ -39,21 +47,25 @@ void loop() {
             // read the incoming byte:
             incomingByte = Serial.read();
             switch (incomingByte) {
-              case 49:
+              case 49: // 1
                 Serial.println("Push pin down");
                 myservo.write(10);
                 break;
-              case 50:
+              case 50: // 2
                 Serial.println("Pull pin up");
                 myservo.write(90);
                 break;
-              case 51:
+              case 51: // 3
                 Serial.println("Self Test");
                 myservo.write(120);
                 break;
-              case 52:
-                Serial.println("Alarm Release & Touch SW Mode");
+              case 52: // 4
+                Serial.println("Touch SW Mode (M119)");
                 myservo.write(60);
+                break;
+              case 53: // 5
+                Serial.println("Alarm Release & Reset");
+                myservo.write(160);
                 break;
             }
     }
@@ -63,4 +75,3 @@ void touch() {
   Serial.println("Touch");
   state = !state;
 }
-
--- README.md
+++ README.md
@@ -1,27 +1,50 @@
-# BLTouch-Tester
+# BLTouch/CRTouch -Tester
 Simple Arduino code to test a BTTouch 3D printer sensor.
 
-After a lot of frustration trying to get a BLTouch and a clone (3DTouch) device working, I decided it would be useful to have a simple circuit to test the device to be sure it was a problem with the printer and not the device.
+After a lot of frustration trying to get a BLTouch and a clone (3DTouch, CrTouch) device working, I decided it would be useful to have a simple circuit to test the device to be sure it was a problem with the printer and not the device.
 
 This code is based on a simple Arduino program running on an Arduino ProMini but would be easily transportable to the Uno or any othe flavour.
 
 The hardware for this system is hooked up as follows :
 
 <TABLE>
+  <TH>BLTouch</TH>
   <TR>
-    <TH>PIN</TH><TH>Use</TH>
+    <TH>Arduino PIN</TH><TH>Use</TH>
   </TR>
   <TR>
-    <TD>2</TD><TD>BLTouch - White (Sensor output)</TD>
+    <TD>2</TD><TD>White (Sensor output)</TD>
   </TR>
   <TR>
-    <TD>9</TD><TD>BLTouch - Orange (Signal)</TD>
+    <TD>9</TD><TD>Orange (Signal)</TD>
   </TR>
   <TR>
-    <TD>5V</TD><TD>BLTouch - Red</TD>
+    <TD>5V</TD><TD>Red</TD>
   </TR>
   <TR>
-    <TD>Gnd/0V</TD><TD>BLTouch - Brown</TD>
+    <TD>Gnd/0V</TD><TD>Brown</TD>
+  </TR>
+</TABLE>
+
+<TABLE>
+  <TH>CrTouch</TH>
+  <TR>
+    <TH>Arduino PIN</TH><TH>Use CRTouch (wire from left to right)</TH>
+  </TR>
+  <TR>
+    <TD>2</TD><TD>Blue (Sensor output)</TD>
+  </TR>
+  <TR>
+    <TD>Gnd</TD><TD>Red</TD>
+  </TR>
+  <TR>
+    <TD>9</TD><TD>Yellow (Signal)</TD>
+  </TR>
+  <TR>
+    <TD>5V</TD><TD>Black</TD>
+  </TR>
+  <TR>
+    <TD>Gnd</TD><TD>White</TD>
   </TR>
 </TABLE>
 
@@ -44,6 +67,9 @@ Open serial monitor and send the following to test.
     <TD>3</TD><TD>Self Test</TD>
   </TR>
   <TR>
-    <TD>4</TD><TD>Reset</TD>
+    <TD>4</TD><TD>Touch SW Mode (M119)</TD>
+  </TR>
+  <TR>
+    <TD>5</TD><TD>Alarm Release & Reset</TD>
   </TR>
 </TABLE>
